cc9fe2f275a5c1c2ddddadf066f7a4933f8cf474
Rubén Padilla <rubbenpad2@gmail.com>
2020-10-17 21:48:16 -0500

    [add] starting navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+    <nav>
+        <v-toolbar flat app>
+            <v-toolbar-title class="text-uppercase grey--text">
+                <span class="font-weight-light">good</span>
+                <span>food</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+        </v-toolbar>
+    </nav>
+</template>
