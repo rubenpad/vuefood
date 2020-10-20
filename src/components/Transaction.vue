980cd59b5173541acd267e1f43fdfaa75e3b77aa
Rubén Padilla <rubbenpad2@gmail.com>
2020-10-19 21:21:48 -0500

    [add] formattersMixin

--- src/components/Transaction.vue
+++ src/components/Transaction.vue
@@ -1,77 +1,83 @@
 <template>
-  <v-dialog v-model="dialog" persistent max-width="580px" max-height="4000px">
-    <template v-slot:activator="{ on, attrs }">
-      <v-list-item-content v-bind="attrs" v-on="on">
-        <v-list-item-title v-text="transaction.id"></v-list-item-title>
-      </v-list-item-content>
-    </template>
-    <v-card>
-      <v-simple-table>
-        <template v-slot:default>
-          <thead>
-            <tr>
-              <th class="text-left">
-                Transaction ID
-              </th>
-              <th class="text-left">
-                Date
-              </th>
-              <th class="text-left">
-                Device
-              </th>
-              <th class="text-left">
-                IP
-              </th>
-              <th class="text-left"></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{{ transaction.id }}</td>
-              <td>{{ date }}</td>
-              <td>{{ transaction.device }}</td>
-              <td>{{ transaction.from.ip }}</td>
-            </tr>
-          </tbody>
+    <v-dialog v-model="dialog" persistent max-width="580px" max-height="4000px">
+        <template v-slot:activator="{ on, attrs }">
+            <v-list-item-content v-bind="attrs" v-on="on">
+                <v-list-item-title v-text="transaction.id"></v-list-item-title>
+            </v-list-item-content>
         </template>
-      </v-simple-table>
+        <v-card>
+            <v-simple-table>
+                <template v-slot:default>
+                    <thead>
+                        <tr>
+                            <th class="text-left">
+                                Transaction ID
+                            </th>
+                            <th class="text-left">
+                                Date
+                            </th>
+                            <th class="text-left">
+                                Device
+                            </th>
+                            <th class="text-left">
+                                IP
+                            </th>
+                            <th class="text-left"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{{ transaction.id }}</td>
+                            <td>{{ formatDate(date) }}</td>
+                            <td>{{ transaction.device }}</td>
+                            <td>{{ transaction.from.ip }}</td>
+                        </tr>
+                    </tbody>
+                </template>
+            </v-simple-table>
 
-      <v-simple-table>
-        <template v-slot:default>
-          <thead>
-            <tr>
-              <th class="text-left">
-                Product Name
-              </th>
-              <th class="text-left">
-                Price
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="product in transaction.products" :key="product.id">
-              <td>{{ product.name }}</td>
-              <td>{{ product.price }}</td>
-            </tr>
-          </tbody>
-        </template>
-      </v-simple-table>
+            <v-simple-table>
+                <template v-slot:default>
+                    <thead>
+                        <tr>
+                            <th class="text-left">
+                                Product Name
+                            </th>
+                            <th class="text-left">
+                                Price
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr
+                            v-for="product in transaction.products"
+                            :key="product.id"
+                        >
+                            <td>{{ product.name }}</td>
+                            <td>{{ formatPrice(product.price) }}</td>
+                        </tr>
+                    </tbody>
+                </template>
+            </v-simple-table>
 
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn color="error darken-1" text @click="dialog = false">
-          Close
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="error darken-1" text @click="dialog = false">
+                    Close
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
 </template>
 
 <script>
+import formattersMixin from '@/mixins/formatters';
+
 export default {
-  props: ["transaction", "date"],
-  data() {
-    return { dialog: false };
-  }
+    mixins: [formattersMixin],
+    props: ['transaction', 'date'],
+    data() {
+        return { dialog: false };
+    },
 };
 </script>
---
+++ src/mixins/formatters.js
@@ -0,0 +1,23 @@
+import { format } from 'date-fns';
+import fromUnixTime from 'date-fns/fromUnixTime';
+
+const formattersMixin = {
+    methods: {
+        formatDate(date) {
+            const intDate = parseInt(date);
+            const formattedDate = format(
+                fromUnixTime(intDate),
+                'dd MMMMMMMM yyyy'
+            );
+            return formattedDate;
+        },
+        formatPrice(price) {
+            return Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD',
+            }).format(price.toFixed(0));
+        },
+    },
+};
+
+export default formattersMixin;
