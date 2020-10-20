6cbdc4f42a2e4c66a9fefef6edde8e7a1eb9a33a
Rubén Padilla <rubbenpad2@gmail.com>
2020-10-19 20:33:43 -0500

    [update] Buyers.vue change to buyers table

--- src/views/Buyers.vue
+++ src/views/Buyers.vue
@@ -1,73 +1,100 @@
 <template>
-  <ul class="grid-content">
-    <Buyer v-for="buyer in buyers" :key="buyer.id" v-bind:buyer="buyer" />
-  </ul>
+    <v-card width="860px" max-width="100%" class="mx-auto">
+        <v-simple-table>
+            <template v-slot:default>
+                <thead>
+                    <tr>
+                        <th class="text-left">
+                            Buyer ID
+                        </th>
+                        <th class="text-left">
+                            Buyer Name
+                        </th>
+                        <th class="text-left">
+                            Buyer Age
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr
+                        class="buyer"
+                        v-for="buyer in buyers"
+                        :key="buyer.id"
+                        @click="goToBuyer(buyer.id)"
+                    >
+                        <td>{{ buyer.id }}</td>
+                        <td>{{ buyer.name }}</td>
+                        <td>{{ buyer.age }}</td>
+                    </tr>
+                </tbody>
+            </template>
+        </v-simple-table>
+    </v-card>
 </template>
 
 <script>
-import Buyer from "@/components/Buyer.vue";
-
 export default {
-  components: { Buyer },
-  data() {
-    return {
-      buyers: [
-        {
-          id: 1,
-          name: "One",
-          age: 23
+    data() {
+        return {
+            buyers: [
+                {
+                    id: 1,
+                    name: 'One',
+                    age: 23,
+                },
+                {
+                    id: 2,
+                    name: 'One',
+                    age: 23,
+                },
+                {
+                    id: 3,
+                    name: 'One',
+                    age: 23,
+                },
+                {
+                    id: 4,
+                    name: 'One',
+                    age: 23,
+                },
+                {
+                    id: 5,
+                    name: 'One',
+                    age: 23,
+                },
+                {
+                    id: 6,
+                    name: 'One',
+                    age: 23,
+                },
+                {
+                    id: 7,
+                    name: 'One',
+                    age: 23,
+                },
+                {
+                    id: 8,
+                    name: 'One',
+                    age: 23,
+                },
+                {
+                    id: 9,
+                    name: 'One',
+                    age: 23,
+                },
+            ],
+        };
+    },
+    methods: {
+        goToBuyer(buyerId) {
+            this.$router.push({ name: 'History', params: { buyerId } });
         },
-        {
-          id: 2,
-          name: "One",
-          age: 23
-        },
-        {
-          id: 3,
-          name: "One",
-          age: 23
-        },
-        {
-          id: 4,
-          name: "One",
-          age: 23
-        },
-        {
-          id: 5,
-          name: "One",
-          age: 23
-        },
-        {
-          id: 6,
-          name: "One",
-          age: 23
-        },
-        {
-          id: 7,
-          name: "One",
-          age: 23
-        },
-        {
-          id: 8,
-          name: "One",
-          age: 23
-        },
-        {
-          id: 9,
-          name: "One",
-          age: 23
-        }
-      ]
-    };
-  }
+    },
 };
 </script>
 
 <style>
-.grid-content {
-  display: grid;
-  grid-gap: 15px;
-  padding: 1.5rem;
-  grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+.buyer {
+    cursor: pointer;
 }
 </style>
