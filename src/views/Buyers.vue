87b9f92c730c2b442a72875719bc39529cb4937c
Rubén Padilla <rubbenpad2@gmail.com>
2020-10-20 09:57:13 -0500

    [add] spinner to show while get data

--- src/views/Buyers.vue
+++ src/views/Buyers.vue
@@ -1,43 +1,49 @@
 <template>
-  <v-card width="860px" max-width="100%" class="mx-auto">
-    <v-simple-table>
-      <template v-slot:default>
-        <thead>
-          <tr>
-            <th class="text-left">
-              Buyer ID
-            </th>
-            <th class="text-left">
-              Buyer Name
-            </th>
-            <th class="text-left">
-              Buyer Age
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            class="buyer"
-            v-for="buyer in buyers"
-            :key="buyer.id"
-            @click="goToBuyer(buyer.id)"
-          >
-            <td>{{ buyer.id }}</td>
-            <td>{{ buyer.name }}</td>
-            <td>{{ buyer.age }}</td>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
-  </v-card>
+  <div>
+    <Spinner v-if="isLoading" />
+    <v-card v-if="!isLoading" width="860px" max-width="100%" class="mx-auto">
+      <v-simple-table>
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th class="text-left">
+                Buyer ID
+              </th>
+              <th class="text-left">
+                Buyer Name
+              </th>
+              <th class="text-left">
+                Buyer Age
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              class="buyer"
+              v-for="buyer in buyers"
+              :key="buyer.id"
+              @click="goToBuyer(buyer.id)"
+            >
+              <td>{{ buyer.id }}</td>
+              <td>{{ buyer.name }}</td>
+              <td>{{ buyer.age }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </v-card>
+  </div>
 </template>
 
 <script>
+import Spinner from "@/components/Spinner.vue";
 import api from "@/api";
 
 export default {
+  components: { Spinner },
   data() {
     return {
+      isLoading: false,
       buyers: []
     };
   },
@@ -46,8 +52,10 @@ export default {
       this.$router.push({ name: "History", params: { buyerId } });
     },
     async getBuyers() {
+      this.isLoading = true;
       const response = await api.getBuyers();
       this.buyers = response.data.buyers;
+      this.isLoading = false;
     }
   },
   created() {
--- src/views/History.vue
+++ src/views/History.vue
@@ -1,80 +1,87 @@
 <template>
-  <div class="grid-products">
-    <div class="grid-up">
-      <v-card>
-        <v-simple-table>
-          <template v-slot:default>
-            <thead>
-              <tr>
-                <th class="text-left">
-                  Buyer ID
-                </th>
-                <th class="text-left">
-                  Buyer Name
-                </th>
-                <th class="text-left">
-                  Age
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>{{ buyer.id }}</td>
-                <td>{{ buyer.name }}</td>
-                <td>{{ buyer.age }}</td>
-              </tr>
-            </tbody>
-          </template>
-        </v-simple-table>
+  <div>
+    <Spinner v-if="isLoading" />
+    <div class="grid-products" v-if="!isLoading">
+      <div class="grid-up">
+        <v-card>
+          <v-simple-table>
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th class="text-left">
+                    Buyer ID
+                  </th>
+                  <th class="text-left">
+                    Buyer Name
+                  </th>
+                  <th class="text-left">
+                    Age
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{{ buyer.id }}</td>
+                  <td>{{ buyer.name }}</td>
+                  <td>{{ buyer.age }}</td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+        </v-card>
+      </div>
+
+      <v-card class="grid-down" tile>
+        <v-list>
+          <v-subheader class="text-subtitle-1"
+            >Transactions by date:</v-subheader
+          >
+          <v-list-group v-for="report in history" :key="report.date">
+            <template v-slot:activator>
+              <v-icon class="mr-4" color="primary">mdi-calendar</v-icon>
+              <v-list-item-content>
+                <v-list-item-title
+                  v-text="formatDate(report.date)"
+                ></v-list-item-title>
+              </v-list-item-content>
+            </template>
+
+            <v-list-item
+              v-for="transaction in report.transactions"
+              :key="transaction.id"
+              class="ml-6"
+            >
+              <v-icon class="mr-4" color="info">mdi-tag</v-icon>
+              <Transaction
+                v-bind:transaction="transaction"
+                v-bind:date="report.date"
+              />
+            </v-list-item>
+          </v-list-group>
+        </v-list>
+      </v-card>
+
+      <v-card class="grid-right" tile>
+        <Suggestions v-bind:suggestions="suggestions" />
       </v-card>
     </div>
-
-    <v-card class="grid-down" tile>
-      <v-list>
-        <v-subheader class="text-subtitle-1">Transactions by date:</v-subheader>
-        <v-list-group v-for="report in history" :key="report.date">
-          <template v-slot:activator>
-            <v-icon class="mr-4" color="primary">mdi-calendar</v-icon>
-            <v-list-item-content>
-              <v-list-item-title
-                v-text="formatDate(report.date)"
-              ></v-list-item-title>
-            </v-list-item-content>
-          </template>
-
-          <v-list-item
-            v-for="transaction in report.transactions"
-            :key="transaction.id"
-            class="ml-6"
-          >
-            <v-icon class="mr-4" color="info">mdi-tag</v-icon>
-            <Transaction
-              v-bind:transaction="transaction"
-              v-bind:date="report.date"
-            />
-          </v-list-item>
-        </v-list-group>
-      </v-list>
-    </v-card>
-
-    <v-card class="grid-right" tile>
-      <Suggestions v-bind:suggestions="suggestions" />
-    </v-card>
   </div>
 </template>
 
 <script>
 import Transaction from "@/components/Transaction.vue";
 import Suggestions from "@/components/Suggestions.vue";
+import Spinner from "@/components/Spinner.vue";
 import formattersMixin from "@/mixins/formatters";
 import api from "@/api";
 
 export default {
   mixins: [formattersMixin],
-  components: { Transaction, Suggestions },
+  components: { Transaction, Suggestions, Spinner },
   data() {
     return {
       dialog: false,
+      isLoading: false,
       buyer: {},
       history: [],
       suggestions: []
@@ -85,11 +92,13 @@ export default {
   },
   methods: {
     async getBuyerHistory() {
+      this.isLoading = true;
       const buyerId = this.$route.params.buyerId;
       const response = await api.getBuyerHistory(buyerId);
       this.buyer = response.data.buyer[0];
       this.history = response.data.history;
       this.suggestions = response.data.suggestions;
+      this.isLoading = false;
     }
   }
 };
@@ -100,7 +109,7 @@ export default {
   display: grid;
   grid-gap: 12px;
   grid-template-columns: 60% 40%;
-  grid-template-rows: 5% 95%;
+  grid-template-rows: 3% auto;
 }
 
 .grid-up {
